c67fe6d7ba2b110278d18685876b9cf94ffb406a
jtmar28 <justinmartinez0123@gmail.com>
2018-11-13 14:31:26 -0500

    function addTask was added

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ function loadAllEventHandlers(){
 }
 
 function addTask(e){
-    
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    li.appendChild(document.createTextNode(task.value));
+    const a = document.createElement('a');
+    a.className ='delete-item secondary-content';
+    a.innerHTML = '<i class="fa fa-trash"></i>';
+    li.appendChild(a);
+    list.appendChild(li);
     e.preventDefault();
 }
